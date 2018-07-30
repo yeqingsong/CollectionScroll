41d564f29e83281345b9b538095cd28c0a546ef4
shoule <253281479@qq.com>
2018-07-30 10:24:28 +0800

    no message

---
+++ CollectionScroll/CollectionScroll/sadaTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  sadaTableViewCell.m
+//  CollectionScroll
+//
+//  Created by shoule on 2018/7/30.
+//  Copyright © 2018年 yqs. All rights reserved.
+//
+
+#import "sadaTableViewCell.h"
+
+@implementation sadaTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
